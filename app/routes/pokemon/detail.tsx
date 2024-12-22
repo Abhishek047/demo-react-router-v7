8ab8651d2f75744582b38d51e07ca819757d6c96
Abhishek047 <abhi.shukla0047@gmail.com>
2024-12-22 18:21:53 +0530

    Devployed

--- app/routes/pokemon/detail.tsx
+++ app/routes/pokemon/detail.tsx
@@ -25,82 +25,87 @@ const STAT_COLOR = {
 };
 
 const Page = ({ loaderData: { data } }: Route.ComponentProps) => {
-    return data ? (
-        <div className='grid grid-cols-4'>
-            <div className='col-span-1'>
-                <div className='flex flex-col justify-center items-center gap-8'>
-                    {Object.values(data.sprites)
-                        .filter((url) => !!url && typeof url === "string")
-                        .map((url) => (
-                            <div className='p-4 border-indigo-200 border-[1px] cursor-pointer rounded-md'>
-                                <img src={url as unknown as string} alt='' />
-                            </div>
-                        ))}
-                </div>
-            </div>
-            <div className='col-span-3'>
-                <div>
-                    <p className='text-5xl capitalize'>{data.name}</p>
-                </div>
-                <div className='my-8 grid grid-cols-4 gap-2 w-full rounded-md p-4 bg-indigo-800/30'>
-                    <div className='dataInfo-page'>
-                        <p className='cardInfo-page'>{data.base_experience}</p>
-                        <p className='cardInfo-name-page'>Exp</p>
-                    </div>
-                    <div className='dataInfo-page'>
-                        <p className='cardInfo-page'>{data.moves.length}</p>
-                        <p className='cardInfo-name-page'>Moves</p>
-                    </div>
-                    <div className='dataInfo-page'>
-                        <p className='cardInfo-page'>{data.height}</p>
-                        <p className='cardInfo-name-page'>Height</p>
-                    </div>
-                    <div className='dataInfo-page'>
-                        <p className='cardInfo-page'>{data.weight}</p>
-                        <p className='cardInfo-name-page'>Weight</p>
-                    </div>
-                </div>
-                <div>
-                    <p className='section-heading'>Abilities</p>
-                    <ul className='pl-4'>
-                        {data.abilities.map((item) => (
-                            <li>
-                                <p className='capitalize mb-1'>{item.ability.name}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className='my-8'>
-                    <p className='section-heading'>Stats</p>
-                    <ul className='pl-4'>
-                        {data.stats.map((item) => (
-                            <li className='flex flex-col'>
-                                <p className='capitalize flex items-center gap-4 mb-4'>
-                                    {item.stat.name}{" "}
-                                    <span className='cardInfo-page'>{item.base_stat}</span>
-                                </p>
-                                <div className='w-full mb-4'>
-                                    <div className='bg-stroke dark:bg-dark-3 relative h-1.5 w-full rounded-2xl'>
-                                        <div
-                                            className={`${
-                                                STAT_COLOR[
-                                                    item.stat.name as keyof typeof STAT_COLOR
-                                                ] || "bg-blue-300"
-                                            } absolute top-0 left-0 h-full rounded-2xl`}
-                                            style={{
-                                                width: `${item.base_stat}%`,
-                                            }}
-                                        />
+    return (
+        <section>
+            {data ? (
+                <div className='grid grid-cols-4'>
+                    <div className='col-span-1'>
+                        <div className='flex flex-col justify-center items-center gap-8'>
+                            {Object.values(data.sprites)
+                                .filter((url) => !!url && typeof url === "string")
+                                .map((url) => (
+                                    <div className='p-4 border-indigo-200 border-[1px] cursor-pointer rounded-md'>
+                                        <img src={url as unknown as string} alt='' />
                                     </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                ))}
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div>
+                            <p className='text-5xl capitalize'>{data.name}</p>
+                        </div>
+                        <div className='my-8 grid grid-cols-4 gap-2 w-full rounded-md p-4 bg-indigo-800/30'>
+                            <div className='dataInfo-page'>
+                                <p className='cardInfo-page'>{data.base_experience}</p>
+                                <p className='cardInfo-name-page'>Exp</p>
+                            </div>
+                            <div className='dataInfo-page'>
+                                <p className='cardInfo-page'>{data.moves.length}</p>
+                                <p className='cardInfo-name-page'>Moves</p>
+                            </div>
+                            <div className='dataInfo-page'>
+                                <p className='cardInfo-page'>{data.height}</p>
+                                <p className='cardInfo-name-page'>Height</p>
+                            </div>
+                            <div className='dataInfo-page'>
+                                <p className='cardInfo-page'>{data.weight}</p>
+                                <p className='cardInfo-name-page'>Weight</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='section-heading'>Abilities</p>
+                            <ul className='pl-4'>
+                                {data.abilities.map((item) => (
+                                    <li>
+                                        <p className='capitalize mb-1'>{item.ability.name}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className='my-8'>
+                            <p className='section-heading'>Stats</p>
+                            <ul className='pl-4'>
+                                {data.stats.map((item) => (
+                                    <li className='flex flex-col'>
+                                        <p className='capitalize flex items-center gap-4 mb-4'>
+                                            {item.stat.name}{" "}
+                                            <span className='cardInfo-page'>{item.base_stat}</span>
+                                        </p>
+                                        <div className='w-full mb-4'>
+                                            <div className='bg-stroke dark:bg-dark-3 relative h-1.5 w-full rounded-2xl'>
+                                                <div
+                                                    className={`${
+                                                        STAT_COLOR[
+                                                            item.stat
+                                                                .name as keyof typeof STAT_COLOR
+                                                        ] || "bg-blue-300"
+                                                    } absolute top-0 left-0 h-full rounded-2xl`}
+                                                    style={{
+                                                        width: `${item.base_stat}%`,
+                                                    }}
+                                                />
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    ) : (
-        <NotFound />
+            ) : (
+                <NotFound />
+            )}
+        </section>
     );
 };
 
